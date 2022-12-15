37c6c876bb6768d2fd3cea908b47b646ef59fd45
shouya31 <shouya0331@ezweb.ne.jp>
2022-12-15 22:28:52 +0900

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'render/index'
  
   # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
-  root 'render#index'end
+  root 'render#index'
 end
