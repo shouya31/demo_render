085a41a233dc14d44a8c44bb4d21afdce513c4a1
shouya31 <shouya0331@ezweb.ne.jp>
2022-12-15 22:24:17 +0900

    Add Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'render/index'
+ 
+  # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
+  root 'render#index'end
 end
